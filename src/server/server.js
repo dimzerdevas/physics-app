e7e599adfe629ad9b29a5d2ca19598930b144f2c
Dimitris Zerdevas <dimitrisz@expressdigibooks.com>
2021-01-06 15:19:33 +0200

    server file added

---
+++ src/server/server.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+var publicPath = path.join(__dirname, '..', 'public');
+var port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log('Server is up!');
+});
